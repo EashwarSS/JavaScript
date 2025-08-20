309f05d62eb79ad3620ff51030f7e1d3aac7dc40
Kadavala Eashwar Sundhar Sai <eashwarlucky.com>
2025-08-20 13:59:39 +0530

    For_while_loop

---
+++ Intermediate/for_loop.js
@@ -0,0 +1,31 @@
+var output = [];
+// var count = 1;
+
+function fizzBuzz() {
+
+    for(var count = 1; count < 101; count++) {
+        if (count % 3 === 0 && count % 5 === 0) {
+            output.push("FizzBuzz");
+        } else if (count % 3 === 0) {
+            output.push("Fizz");
+        } else if (count % 5 === 0) {
+            output.push("Buzz");
+        } else {
+            output.push(count);
+        }
+            // count++;
+    }
+
+    console.log(output);
+}
+
+// Difference
+// State
+// while (something is true) {
+//     do something
+// }
+
+// Iterate
+// for (i=0; i<2; i++) {
+//     do something
+// }
