83d6460df8cc77785a2167b84da4e649eaa8f259
Kadavala Eashwar Sundhar Sai <eashwarlucky.com>
2025-08-22 23:28:02 +0530

    Advanced_Java

---
+++ Advanced/audio_play.js
@@ -0,0 +1,12 @@
+function Audio (fileLocation) {
+    this.fileLocation = fileLocation;
+    this.play = function() {
+        // Tap into the audio hardware
+        // Check the file at fileLocation exists
+        // Check the file at fileLocation is a sound file
+        // Play the file at fileLocation
+    }
+}
+
+// var audio1 = new audio("sounds/audio-1.mp3");
+// audio1.play();
---
+++ Advanced/userdata.js
@@ -0,0 +1,8 @@
+var userData = {
+    yearsOfExperience: 5,
+    name: "Eashwar",
+    skills: ["Devops", "Webdesign", "Cloud"],
+}
+
+// userData;
+// console.log(userData.name)
